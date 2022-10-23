05e99d64a113764c690c10481c9d46c352e46464
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2022-10-23 22:08:04 +0200

    create certification

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./User');
 const Course = require('./Course');
 const Wallet = require('./WalletModel');
+const CertificationModel = require('./CertificationModel')
 
 module.exports = {
   User,
   Course,
-  Wallet
+  Wallet,
+  CertificationModel
 };
