2b3f717c92af1f2471f0a7ea81f1f694781905ea
ahmed elblatgy <ahmedelblatgy125@gmail.com>
2022-10-29 23:04:23 +0200

    add new models Corporate Corporatetrainee Studnetanswer video

---
+++ backend/models/Video.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const VideoSchame = mongoose.Schema({
+Title:{
+    type: String,
+    require:true,
+} ,
+related_Course:{
+type:Schema.Types.ObjectId,
+ref:"Course",
+require:true,
+},
+Video_Description:{
+type:String,
+require:true,
+},
+Link:{
+type:String,
+},
+viewed:{
+type:Boolean,
+},
+VideoNode:{
+    type:Schema.Types.ObjectId,
+    ref:"VideoNotesModel.js"
+}
+
+
+
+
+});
+
+module.exports = mongoose.model('Video', VideoSchame);
