dbf1a9bbece36335a7956764f67fe5663be85dab
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2022-12-28 19:52:33 +0200

    course request

--- backend/routes/RequestRouter.js
+++ backend/routes/RequestRouter.js
@@ -4,12 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 const  {addRequest,getAllRequest,getRequests} = require('../controllers/RequestCourseController');
-const {authAdmin} = require('../middlewares');
+const {authAdmin,authMiddleware} = require('../middlewares');
 
 
-router.post('/addRequest/:courseId',addRequest);
-router.get('/getAllRequest',authAdmin,getAllRequest);
-router.get('/getRequests',getRequests);
+router.post('/addRequest/:courseId',authMiddleware,addRequest);
+router.get('/getAllRequest',authMiddleware,authAdmin,getAllRequest);
+router.get('/getRequests/:courseId',authMiddleware,getRequests);
 
 
 module.exports = router;
